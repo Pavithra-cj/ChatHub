799117176bab460be46b5b24bef7a123105f1783
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2025-07-26 00:16:46 +0530

    Initial Commit

---
+++ ChatHub/ChatHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatHubApp.swift
+//  ChatHub
+//
+//  Created by Pavithra Chamod on 2025-07-26.
+//
+
+import SwiftUI
+
+@main
+struct ChatHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatHubTests/ChatHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatHubTests.swift
+//  ChatHubTests
+//
+//  Created by Pavithra Chamod on 2025-07-26.
+//
+
+import Testing
+@testable import ChatHub
+
+struct ChatHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
