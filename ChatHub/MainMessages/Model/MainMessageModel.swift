daca54618e6b148a6030cc1c80a566eee75ff6ee
Pavithra Chamod J <84024015+Pavithra-cj@users.noreply.github.com>
2025-08-11 06:26:27 +0530

    MainMessage component updated with model, viewModel and view.

---
+++ ChatHub/MainMessages/Model/MainMessageModel.swift
@@ -0,0 +1,16 @@
+//
+//  MainMessageModel.swift
+//  ChatHub
+//
+//  Created by Pavithra Chamod on 2025-08-11.
+//
+
+import Foundation
+import SwiftUI
+
+struct ChatUser{
+    var uid: String
+    var email: String
+    var username: String
+    var profileImage: String?
+}
